e4e71ccdcb108700dba16836b58c09c505b1534f
UwUguasho <inlaureano@gmail.com>
2026-03-30 00:22:46 +0200

    Commented basePath line

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // makes next export work
-  basePath: '/Website-Portfolio', //comment this line for local dev
+  //basePath: '/Website-Portfolio', //comment this line for local dev
 };
 
 module.exports = nextConfig;
